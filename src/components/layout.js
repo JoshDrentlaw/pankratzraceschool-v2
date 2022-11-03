1fe79514a300cfd35f8b5e470e4d45c7c9fb246b
Josh Drentlaw <joshdrentlaw@gmail.com>
2022-11-02 20:57:03 -0700

    More Sanity stuff. Added home schema.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,51 @@
 import React from "react"
+import styled from "styled-components"
 import signature from '../images/signature.png'
 
-const link = {
-    color: 'white',
-    textDecoration: 'none'
-}
+const Body = styled.div`
+    @font-face {
+        font-family: 'Inter var';
+        font-weight: 100 900;
+        font-display: swap;
+        font-style: normal;
+        font-named-instance: 'Regular';
+        src: url(/fonts/Inter-roman.var.woff2) format('woff2');
+    }
+
+    * {
+        font-family: 'Inter var';
+    }
+`
+
+const Link = styled.a`
+    color: white;
+    text-decoration: none;
+    font-size: 1.5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 100px;
+    padding-inline: 15px;
+
+    &:hover {
+        color: #333;
+        background-color: white;
+    }
+`
 
 export default function Layout({ children }) {
     return (
-        <>
+        <Body>
             <nav style={{backgroundColor: '#333', marginInline: '-8px', marginTop: '-8px', display: 'flex', justifyContent: 'space-between'}}>
                 <img src={signature} alt="Wally Pankratz signature" style={{ marginInline: '10px' }} />
                 <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                    <a href="/" style={link}>Home</a>
-                    <a href="/about" style={link}>About</a>
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
                 </div>
             </nav>
             <main>
                 { children }
             </main>
-        </>
+        </Body>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
 import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import BlockContent from "@sanity/block-content-to-react"
 
-const headingStyles = {
-    marginTop: 0,
-    marginBottom: 64,
+const H1 = styled.h1`
+    text-align: center;
+    font-size: 3em;
+`
+
+const BlockContainer = styled.section`
+    text-align: center;
+    width: 50%;
+    margin-inline: auto;
+
+    p > strong {
+        font-size: x-large;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+`
+
+const serializers = {
+    types: {
+        code: props => (
+            <pre data-language={props.node.language}>
+                <code>{ props.node.code }</code>
+            </pre>
+        )
+    }
 }
 
 const IndexPage = () => {
@@ -12,16 +39,19 @@ const IndexPage = () => {
         {
             sanityHome {
                 heading
-                body {
-                    _rawChildren
-                }
+                _rawBody
             }
         }
     `)
 
+    console.log(data)
+
     return (
         <Layout>
-            <h1 style={headingStyles}>{ data.sanityHome.heading }</h1>
+            <H1>{data.sanityHome.heading}</H1>
+            <BlockContainer>
+                <BlockContent blocks={data.sanityHome._rawBody} serializers={serializers} />
+            </BlockContainer>
         </Layout>
     )
 }
